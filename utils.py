03ced4de4e86c7fb3bc1a4d3894cb1af3a85b162
jFompe <jaime.fompe@gmail.com>
2022-01-04 19:49:33 +0100

    in_range in utils

--- utils.py
+++ utils.py
@@ -3,3 +3,6 @@ def get_categorical_variables(df):
 
 def get_numerical_variables(df):  
   return [var for var in df if df[var].dtypes != object]
+
+def in_range(col, min_val, max_val):
+  return (col >= min_val) & (col <= max_val)
